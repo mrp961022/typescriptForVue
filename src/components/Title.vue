febf41a4a780e6f3f93a53ac79828522f7823276
maruiping <13833589337@163.com>
2019-12-16 14:53:29 +0800

    增加忽略项

--- src/components/Title.vue
+++ src/components/Title.vue
@@ -1,8 +1,19 @@
 <template>
-    
+    <div>{{ msg }}</div>
 </template>
-<script>
-export default {
-    
+<script lang="ts">
+import Vue from "vue";
+import Component from "vue-class-component";
+// @Component 修饰符注明了此类为一个 Vue 组件
+@Component({
+    name: "title"
+})
+export default class Hello extends Vue {
+    // 初始数据可以直接声明为实例的属性
+    msg: string = "我是项目的title";
+    // 组件方法也可以直接声明为实例的方法
+    sayHello(): void {
+        console.log(this.msg);
+    }
 }
 </script>
