af735dede8797a8ee4b470b098548397119cc336
maruiping <13833589337@163.com>
2019-12-17 09:12:11 +0800

    提交一下

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,23 +1,28 @@
 <template>
-  <div class="hello">
-    <h1 @click="sayHello">{{ msg }}</h1>
-  </div>
+    <div class="hello">
+        <h1 @click="msg=changeMsg()">{{ msg }}</h1>
+        <newTitle :value="msg" />
+    </div>
 </template>
 
 <script lang="ts">
-  import Vue from 'vue'
-  import Component from 'vue-class-component'
-  // @Component 修饰符注明了此类为一个 Vue 组件
-  @Component({
-    name:'hello'
-  })
-  export default class Hello extends Vue {
-    // 初始数据可以直接声明为实例的属性
-    msg: string ='这是一个vue+typescript项目'
-    // 组件方法也可以直接声明为实例的方法
-    sayHello ():void {
-      console.log(this.msg)
+import Vue from "vue";
+import Component from "vue-class-component";
+// @Component 修饰符注明了此类为一个 Vue 组件
+import newTitle from "./newTitle.vue";
+@Component({
+    name: "hello",
+    components: {
+        newTitle
     }
-  }
+})
+export default class Hello extends Vue {
+    // 初始数据可以直接声明为实例的属性
+    msg: String = "我是父组件传来的值";
+    // 组件方法也可以直接声明为实例的方法
+    changeMsg(): String {
+        return "我是改变过的msg";
+    }
+}
 </script>
 
---
+++ src/components/newTitle.vue
@@ -0,0 +1,31 @@
+<template>
+    <div @click="sayHello" title="点击alert父组件传来的字符">{{ msg }}</div>
+</template>
+<script lang="ts">
+import Vue from "vue";
+import Component from "vue-class-component";
+// @Component 修饰符注明了此类为一个 Vue 组件
+@Component({
+    name: "newTitle",
+    props: {
+        value: { // 变量名
+            type: String, // 类型
+            required: true // 是否必填
+        },
+        age: Number
+    }, // 父组件传过来的值
+    watch:{ // 监听父组件传来的变量
+        value(val): void{
+            console.log(val)
+        }
+    }
+})
+export default class newTitle extends Vue {
+    // 初始数据可以直接声明为实例的属性
+    msg: string = "我是项目的title";
+    // 组件方法也可以直接声明为实例的方法
+    sayHello(): void {
+        alert(this.$props.value);
+    }
+}
+</script>
