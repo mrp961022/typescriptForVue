50ad6cd2c18ad177c450924712c091f0d0348b01
maruiping <13833589337@163.com>
2020-04-26 14:24:43 +0800

    增加表格预加载（concat模拟查询表格数据，实际情况改为分页即可）

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -2,7 +2,15 @@
     <div class="hello">
         <h1 @click="msg=changeMsg()">{{ msg }}</h1>
         <newTitle :value="msg" />
-        <el-button>这回有样式了吗</el-button>
+        <el-button @click="pushBefore">这回有样式了吗</el-button>
+        <el-table border header-row-class-name="df-header-row" :data="data" height="500">
+            <el-table-column
+                v-for="(item,index) in column"
+                :key="index"
+                :prop="item.value"
+                :label="item.label"
+            ></el-table-column>
+        </el-table>
     </div>
 </template>
 
@@ -20,9 +28,56 @@ import newTitle from "./newTitle.vue";
 export default class Hello extends Vue {
     // 初始数据可以直接声明为实例的属性
     msg: String = "我是父组件传来的值";
+    data: Array<Object> = []; // 定义数组时需要定义数组内容的格式
+    isSearch: Boolean = false;
+    column: Array<Object> = [
+        { value: "name", label: "姓名" },
+        { value: "age", label: "年龄" },
+        { value: "sex", label: "性别" },
+        { value: "num", label: "学号" },
+        { value: "class", label: "年级" }
+    ];
+    // 生命周期直接写就行
+    mounted() {
+        this.pushBefore();
+        let dom: any = document.querySelector(".hello .el-table__body-wrapper"); // 注意不论在哪里，一定要定义类型，如果是dom就定义为any
+        dom.addEventListener("scroll", () => {
+            const scrollDistance =
+                dom.scrollHeight - dom.scrollTop - dom.clientHeight;
+            if (scrollDistance > 0) {
+                this.isSearch = true; // 防止重新查询第一页时出现查询两次的问题
+            }
+            if (scrollDistance == 0 && this.isSearch) {
+                this.query();
+            }
+        });
+    }
     // 组件方法也可以直接声明为实例的方法
     changeMsg(): String {
+        // 返回值类型要在方法后面声明
         return "我是改变过的msg";
+    }
+    // 鼠标点击事件
+    pushBefore(): void {
+        // void就是没有return返回值
+        this.isSearch = false;
+        this.data = [
+            { name: 1, age: 2, sex: "male", num: "0001", class: "1年3班" },
+            { name: 2, age: 3, sex: "feMale", num: "0002", class: "1年2班" },
+            { name: 1, age: 2, sex: "male", num: "0003", class: "1年3班" },
+            { name: 2, age: 3, sex: "male", num: "0004", class: "1年2班" },
+            { name: 1, age: 2, sex: "feMale", num: "0005", class: "1年3班" },
+            { name: 2, age: 3, sex: "feMale", num: "0006", class: "1年3班" },
+            { name: 1, age: 2, sex: "male", num: "0007", class: "1年3班" },
+            { name: 2, age: 3, sex: "feMale", num: "0008", class: "1年4班" },
+            { name: 1, age: 2, sex: "male", num: "0009", class: "1年3班" },
+            { name: 2, age: 3, sex: "male", num: "0010", class: "1年3班" },
+            { name: 1, age: 2, sex: "feMale", num: "0011", class: "1年3班" },
+            { name: 2, age: 3, sex: "male", num: "0012", class: "1年1班" }
+        ];
+    }
+    query(): void {
+        this.data = this.data.concat(this.data);
     }
 }
 </script>
